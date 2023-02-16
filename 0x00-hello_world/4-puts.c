a9c463892b72f4b409d3bfe9e7cf9ba4ba8b8c3c
dimingo <micalvin98@gmail.com>
2023-02-16 06:10:13 -0800

    latest update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
 char Mystr1[] = "\"Programming is like building a multilingual puzzle"; 
