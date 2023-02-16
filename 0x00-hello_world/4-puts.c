5d0c1bfd23fb26ccef1e5029088af8898a311a24
dimingo <micalvin98@gmail.com>
2023-02-16 05:30:13 -0800

    latest update

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	//string initialisation
+    char Mystr1[] = "Programming is like building a multilingual puzzle"; 
+    puts(Mystr1);
+     
+    
+    return 0;
+}
