59e6669c42dfe6427280af15495ff31e5240ab27
dimingo <micalvin98@gmail.com>
2023-02-16 05:58:39 -0800

    latest update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    char Mystr1[] = "Programming is like building a multilingual puzzle"; 
+    char Mystr1[] = "\"Programming is like building a multilingual puzzle"; 
     puts(Mystr1);
      
     
