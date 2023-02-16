142580caecc451cb51760701b8d2fccbb9dd39ca
dimingo <micalvin98@gmail.com>
2023-02-16 08:57:14 -0800

    latest update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
- * */
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
