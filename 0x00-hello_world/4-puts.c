a42f53a51b4d85a910f261cb28b6fd53871283bb
dimingo <micalvin98@gmail.com>
2023-02-16 06:12:13 -0800

    latest update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-char Mystr1[] = "\"Programming is like building a multilingual puzzle"; 
+char Mystr1[]="\"Programming is like building a multilingual puzzle"; 
 puts(Mystr1);        
 return (0);
 }
