7c886e9ab24f71a3a3694da44dde79614c8e6f14
dimingo <micalvin98@gmail.com>
2023-02-17 10:17:32 -0800

    Latest Upload

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase,
+ * main - prints the lowercase alphabet in reverse,
  * followed by a new line
  * Return: Always 0 (Success)
  */
