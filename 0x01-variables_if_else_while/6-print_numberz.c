c8cb3670d52577e873ef51e9d75f2c47322b1889
dimingo <micalvin98@gmail.com>
2023-02-17 09:44:11 -0800

    Latest Upload

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print single digits 2,
+ * followed by a new line
+ * Return: Aways 0 (Sucess)
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+    	{
+        printf("%d", i);
+    	}
+	printf("\n");
+	return (0);
+}
