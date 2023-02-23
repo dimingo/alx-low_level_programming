b34f91b21bee777d6dbb3e6ba31e548c6283669a
dimingo <micalvin98@gmail.com>
2023-02-23 05:33:14 -0800

    0x04

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers 0 to 9 exluding 2 and  4
+ * Return: numbers
+ */
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if ((i != '2') || i != '4')
+			_putchar(i);
+	}
+	_putchar('\n');
+}
