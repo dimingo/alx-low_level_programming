73a0e9ef591eca087da84dc1732e2404872576d9
dimingo <micalvin98@gmail.com>
2023-02-23 05:07:39 -0800

    0x04

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers 0 to 9
+ * Return : Numbers
+ */
+void print_numbers(void)
+{
+	for (int i = 0; i < 10 ; i++)
+		_printchar(i);
+	_printchar('\n');
+}
