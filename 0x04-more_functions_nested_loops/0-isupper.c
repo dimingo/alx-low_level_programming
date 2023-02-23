17d14e91d5406aeb8cd4500ff63a311cd55f98d6
dimingo <micalvin98@gmail.com>
2023-02-23 04:26:02 -0800

    0x04

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ *
+ * Return: 0 if upper else 0 ;
+ *
+ */
+int _isupper(int c)
+{
+	return ((c >= 'A' && c <= 'Z'));
+	
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _isdigit(int c);
+
+#endif /* MAIN_H */
