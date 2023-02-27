547745db40fbe01b1538fd6318e153ad43939f94
dimingo <micalvin98@gmail.com>
2023-02-27 11:45:59 -0800

    latest updates

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,6 +10,7 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 void _puts(char *str);
 int _isupper(int c);
