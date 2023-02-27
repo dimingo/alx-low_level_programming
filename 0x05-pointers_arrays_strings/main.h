5d4a25dc7f5c99775e0634825a9776acb6a913cb
dimingo <micalvin98@gmail.com>
2023-02-27 10:34:49 -0800

    0x05

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,7 +6,7 @@
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void print_rev(char *s);
-void rev_string(char *s)
+void rev_string(char *s);
 int _strlen(char *s);
 void _puts(char *str);
 int _isupper(int c);
