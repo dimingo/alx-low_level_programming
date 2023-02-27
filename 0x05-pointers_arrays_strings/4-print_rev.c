adc68842f674a4ac96ff65a92bdd9d9309f80c15
dimingo <micalvin98@gmail.com>
2023-02-27 09:26:26 -0800

    0x05

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_rev - print string in reverse
+ * @s : pointer to s as char
+ * Return: Nothing
+ */
+void print_rev(char *s )
+{
+	while (*str)
+	{
+		printif("%c", *str);
+		str--;
+        }
+	printf("\n");
+}
