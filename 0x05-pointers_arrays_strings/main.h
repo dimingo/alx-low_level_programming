abce79c050a55c60ab329d0eb9de675a252d7f5b
dimingo <micalvin98@gmail.com>
2023-02-27 11:12:13 -0800

    latest commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ void swap_int(int *a, int *b);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
 int _strlen(char *s);
 void _puts(char *str);
 int _isupper(int c);
