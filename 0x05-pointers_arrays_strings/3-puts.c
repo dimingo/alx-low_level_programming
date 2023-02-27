4c7fabab3ad621cd303b5542b8fa2221f42abb02
dimingo <micalvin98@gmail.com>
2023-02-27 06:15:06 -0800

    0x05

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * _puts - prints a string to stdout
+ * @str : takes string pointer as input
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int n;
+
+	for (n = 0; *s != '\0'; n++)
+		pritnf("%p", *s);
+		n++;
+	printf("\n");
+}
