51b568d6061e59d139415063fcaad49967656073
dimingo <micalvin98@gmail.com>
2023-03-01 09:54:56 -0800

    0x06

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
 void swap_int(int *a, int *b);
 void print_rev(char *s);
 void rev_string(char *s);
