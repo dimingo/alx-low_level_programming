918ebf35c9eaa48b9a2296ff87c831289f28e286
dimingo <micalvin98@gmail.com>
2023-03-01 12:18:59 -0800

    0x06

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * cap_string - capitalizes everey word of a string
  * @s: string to modify
@@ -9,15 +10,25 @@ char *cap_string(char *s)
 {
 	int i, j;
 
-	char sep[13 ] = {' ', '\t', '\n', ',', ';', '.','!', '?', '"', '(', ')', '{', '}'};
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
 
-	i = j = 0;
-
-	for (; s[i]  != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
+	{
 		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-			s -= 32
-		for  (j = 0; j < 13; j++)
-			if (s[i] == sep[j])
-				s[i + 1] -= 32;
-	return (a);
+			s[i] -= 32;
+
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == spe[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
+	}
+
+	return (s);
 }
