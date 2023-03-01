203f9c1c19db81ecfcb184b9b2f22b77e38c69b7
dimingo <micalvin98@gmail.com>
2023-03-01 08:50:24 -0800

    0x06

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,12 +9,13 @@
  * Return: a pointer to the resulting string dest
  */
 char *_strncpy(char *dest, char *src, int n)
-{ 	int i;
-	
+{
+	int i;
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 	for ( ; i < n; i++)
 		dest[i] = '\0';
 
-	return dest;
+	return (dest);
 }
