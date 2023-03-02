0577e618dbe7c5e66375ef0784b70c453c4b8548
dimingo <micalvin98@gmail.com>
2023-03-02 13:12:05 -0800

    0x06

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * rot13 - encodes a string in rot13
  * @s: string to be encoded
@@ -9,15 +10,20 @@ char *rot13(char *s)
 {
 	int i, j;
 
-	i = j = 0;
-
 	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
+	{
 		for (j = 0; a[j] != '\0'; j++)
+		{
 			if (s[i] == a[j])
+			{
 				s[i] = b[j];
 				break;
+			}
+		}
+	}
+
 	return (s);
 }
