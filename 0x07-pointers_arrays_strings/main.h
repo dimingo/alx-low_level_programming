0d155c467921ed33917f0b868b62a4ca86a0e097
dimingo <micalvin98@gmail.com>
2023-03-06 11:00:49 -0800

    0x07

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,6 +8,7 @@
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 void print_alphabet_x10(void);
 int _putchar(char c);
 int _islower(int c);
