9b8711a766ea82c7f6ddb5172c365b3a57346f67
dimingo <micalvin98@gmail.com>
2023-03-06 03:31:52 -0800

    0x07

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,24 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+void print_alphabet_x10(void);
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_alphabet(void);
+
+#endif
