eb891315d95265071c0a668fa6269830d6a998e4
dimingo <micalvin98@gmail.com>
2023-03-08 04:31:11 -0800

    0x08

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string to print
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_putchar(*(s - 1);
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
