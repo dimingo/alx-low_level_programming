c7c21457cc70eda0acd13d671cd8fc81791bbe95
dimingo <micalvin98@gmail.com>
2023-03-10 05:36:02 -0800

    0x09 tasks

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of string
+ * @s: number to return the factorial from
+ *
+ * Return: s
+ */
+int _strlen(char *s)
+{
+	return (*s);
+}
---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy string
+ * @dest: pointer to dest
+ * @src: pointer to src
+ * @n: integer n
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+        return (dest);
+}
