3200e698d4608c80995cd329487a1ef24d6e1591
dimingo <micalvin98@gmail.com>
2023-03-10 13:13:01 -0800

    0x09 tasks

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	/*int num1, num2, result;*/
+
+	printf("%d", argc);
+	return (0);
+}
