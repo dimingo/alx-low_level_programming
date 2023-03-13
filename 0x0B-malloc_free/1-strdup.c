936866c1003cf803a6de96190de5ea412e413f43
dimingo <micalvin98@gmail.com>
2023-03-13 09:51:52 -0700

    malloc, free tasks

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup -  copy of the string given in as a parameter
+ * @str: string pointer
+ *
+ * Return: NUll if str is empty or pointer to str
+ */
+char *_strdup(char *str)
+{
+	int len, i;
+	char *str2, *p;
+
+	p = str;
+	len = 0;
+	i = 0;
+
+	while (*p != '\0')
+	{
+		len++;
+		p++;
+	}
+
+	str2 = (char *) malloc(len * sizeof(char));
+	if (str2 == NULL)
+		return (NULL);
+	for (; i < len;  i++)
+	{
+		*(str2 + i) = *str;
+		str++;
+	}
+	return (str2);
+}
