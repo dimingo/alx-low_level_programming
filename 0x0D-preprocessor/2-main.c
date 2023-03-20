7ebf6b06517fc1e703f459e6dddbb7e7bf8e5e25
dimingo <micalvin98@gmail.com>
2023-03-20 05:48:19 -0700

    macros exercise

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - prnt the file name
+ * main - Prints the name of the file of the program
+ *        was compiled from, followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
