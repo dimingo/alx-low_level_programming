dad3f2666d47d024557e195da2ad82bd2f6409ff
dimingo <micalvin98@gmail.com>
2023-04-03 17:24:53 +0300

    print all elements of a linked-list

---
+++ 0-puts_recursion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the list_t list to print
+ *
+ * Return: the number of nodes printed
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t s = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		s++;
+	}
+
+	return (s);
+}
