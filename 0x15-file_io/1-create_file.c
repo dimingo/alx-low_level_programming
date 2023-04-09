497ebc8a78f96ad2aa4f0c03252c52dcc75536ea
dimingo <micalvin98@gmail.com>
2023-04-09 17:20:58 +0300

    differenciate open variable from open system call

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int open, write, length  = 0;
+    int open_, write_, length  = 0;
 
     if (filename == NULL)
         return -1;
@@ -19,10 +19,10 @@ int create_file(const char *filename, char *text_content)
         for (; text_content[length];)
             length++;
     }
-    open = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-    write  = write(open, text_content, length);
-    if (open  == -1 || write == -1)
+    open_ = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+    write_  = write(open_, text_content, length);
+    if (open_  == -1 || write_ == -1)
 		return (-1);
-    close(open);
+    close(open_);
     return (1);
 }
